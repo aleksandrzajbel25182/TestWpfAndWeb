2ea50012ae9d95e833afab9143269c435facd76f
aleksandrzajbel25182 <aleksandrzajbel25182@gmail.com>
2025-01-17 18:19:12 +0700

    Комменатрий

--- ProductDB/Entitys/Phone.cs
+++ ProductDB/Entitys/Phone.cs
@@ -6,6 +6,9 @@
         /// Id
         /// </summary>
         public int Id { get; set; }
+        /// <summary>
+        /// комментарий
+        /// </summary>
         public string Brand { get; set; }
         public string Model { get; set; }
         public decimal Price { get; set; }
