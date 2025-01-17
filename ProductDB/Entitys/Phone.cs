9ad2af0654e41d98460e36f27cc7ffd74163cdea
aleksandrzajbel25182 <aleksandrzajbel25182@gmail.com>
2025-01-17 17:27:32 +0700

    Новые изменения

--- ProductDB/Entitys/Phone.cs
+++ ProductDB/Entitys/Phone.cs
@@ -2,6 +2,9 @@
 {
     public class Phone
     {   
+        /// <summary>
+        /// Id
+        /// </summary>
         public int Id { get; set; }
         public string Brand { get; set; }
         public string Model { get; set; }
