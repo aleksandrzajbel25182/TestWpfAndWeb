845df53f9753884164826b9e989cc4f823133065
aleksandrzajbel25182 <aleksandrzajbel25182@gmail.com>
2025-03-17 10:24:27 +0700

    refactor.Модуль.Опечатка

--- ProductDB/Entitys/Phone.cs
+++ ProductDB/Entitys/Phone.cs
@@ -3,7 +3,7 @@
     public class Phone
     {   
         public int Id { get; set; }
-        public string Brend { get; set; }
+        public string Brand { get; set; }
         public string Model { get; set; }
         public decimal Price { get; set; }
 
